322fd212f23b833936a419917beec68acdf65009
hmq1408 <mq14082003@gmail.com>
2023-12-15 14:33:36 +0700

    station delete successful, format date ok

--- client/src/utils/httpRequest.js
+++ client/src/utils/httpRequest.js
@@ -15,8 +15,17 @@ export const post = async (path, data, option = {}) => {
     return response.data;
 }
 
+// export const put = async (path, data, option = {}) => {
+//     const response = await httpRequest.put(path, data, option);
+//     return response.data;
+// }
 export const put = async (path, data, option = {}) => {
     const response = await httpRequest.put(path, data, option);
+    return response.data;
+}
+
+export const del = async (path, option = {}) => {
+    const response = await httpRequest.delete(path, option);
     return response.data;
 }
 
