d2988168e8ad546d1fd12fcba3dab9d37f32fb5b
hmq1408 <mq14082003@gmail.com>
2023-12-18 00:15:08 +0700

    merging station lead fe and be

--- client/src/services/stationLeadService.js
+++ client/src/services/stationLeadService.js
@@ -1,2 +1,10 @@
 import * as httpRequest from '~/utils/httpRequest';
 
+export const getAllStationLeads = async () => {
+    try {
+        const res = await httpRequest.get('users/station-leads');
+        return res;
+    } catch (error) {
+        console.log(error)
+    }
+}
--- server/routes/station.route.js
+++ server/routes/station.route.js
@@ -7,6 +7,7 @@ const router = require('express').Router();
 
 router.post('/add',verifyToken, authorize([constants.ROLES.BOSS]), stationController.addStation);
 router.get('/', stationController.getStation);
+router.get('/:id', stationController.getStationById);
 router.put('/update/:id', stationController.updateStation);
 router.delete('/delete/:id', stationController.deleteStation);
 
