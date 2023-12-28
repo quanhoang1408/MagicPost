cddb8fc467d813300422c196b9004a4c402f6287
dantran <namdan.tran3003@gmail.com>
2023-12-28 17:59:38 +0700

    add date time util

---
+++ server/utils/dateUtil.js
@@ -0,0 +1,17 @@
+const formatDateTime = (date) => {
+    const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false
+    };
+    
+    return date.toLocaleString('vi-VN', options)
+}
+
+module.exports = {
+    formatDateTime
+}
